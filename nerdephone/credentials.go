719550e1fabccc3cc42f74afe8b715b58f3ca540
saintjeremy <saintjeremy@gmail.com>
2020-12-01 09:38:26 -0800

    struct fix attempts - project imports are deprecated

--- nerdephone/credentials.go
+++ nerdephone/credentials.go
@@ -1,39 +1,8 @@
-//testing go locally
 package main
- 
-import "fmt"
- 
-// func main() {
-// 	fmt.Println("Hi there")
-// }
 
-func main(){
-	fmt.Println("Nerdephone: go-twitter bot v0.01")
-	creds := Credentials{
-		AccessToken: 		os.GetEnv("ACCESS_TOKEN"),
-		AccessTokenSecret:	os.GetEnv("ACCESS_TOKEN_SECRET"),
-		ConsumerKey:		os.GetEnv("CONSUMER_KEY"),
-		ConsumerSecret:		os.GetEnv("CONSUMER_SECRET"),
-	}
-
-	fmt.Printf("%+v\n", creds)
-
-	client, err := getClient(&creds)
-	if err != nil {
-		log.Println("Error getting Twitter Client")
-		log.Println(err)
-	}
-
-	// print out the pointer to the client for now so it does not throw errors
-
-	fmt.Printf("%+v\n", client)
-
-}
-
-// !project imports no longer exist
+// project imports no longer exist - 
 
 import (
-  //project imports
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
@@ -52,16 +21,18 @@ type Credentials struct {
 // and return a pointer to a twitter client or an error
 
 func getClient(creds *Credentials) (*twitter.Client, error) {
+
 	//send comsumer key and secret
 	config := oauth.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	
 	//send access token and access token secret
 	token := oauth.NewToken(creds.AccessToken, creds.AccessTokenSecret)
 
 	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 
+	
 	//verify credentials
-
 	verifyParams := &twitter.AccountVerifyingParams{
 		SkipStatus:		twitter.Bool(true),
 		InclueEmail: 	twitter.Bool(true),
@@ -74,4 +45,6 @@ func getClient(creds *Credentials) (*twitter.Client, error) {
 		return nil, err
 	}
 	log.Printf("Users's ACCOUNT:\n%+v\n", user)
+
+
 }
